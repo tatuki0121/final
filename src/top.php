1cb38420a626821e1bcb524f7d012dfdf03b140d
tatuki0121 <148730025+tatuki0121@users.noreply.github.com>
2024-01-10 10:19:29 +0900

    top.php

---
+++ src/top.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>名産品情報管理メニュー</title>
+</head>
+
+<body>
+
+    <h1>名産品情報管理メニュー</h1>
+
+
+    <form action="list.php" method="get">
+        <button type="submit">一覧</button>
+    </form>
+
+    <form action="register.php" method="get">
+        <button type="submit">登録</button>
+    </form>
+
+    <form action="update.php" method="get">
+        <button type="submit">更新</button>
+    </form>
+
+
+    <form action="delete.php" method="get">
+        <button type="submit">削除</button>
+    </form>
+
+</body>
+
+</html>
